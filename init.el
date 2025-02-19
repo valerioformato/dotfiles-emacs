593656bf1358bfc58857b2fd27e416a8b7f16151
Valerio Formato <valerio.formato@cern.ch>
2025-02-19 14:24:59 +0100

    Add clang-format package

--- init.el
+++ init.el
@@ -2,6 +2,10 @@
 (setq user-full-name "Valerio Formato")
 (setq user-mail-address "valerio.formato@gmail.com")
 
+;; ---------------------------------------------------------
+;; Packages
+;; ---------------------------------------------------------
+
 (package-initialize)
 (require 'package)
 (add-to-list 'package-archives
@@ -15,12 +19,30 @@
   :defer t
   :ensure t
   )
+
 (use-package python
   :defer t
   :ensure t
   :mode ("\\.py\\'" . python-mode)
   :interpreter ("python" . python-mode)
   )
+
+(use-package yaml-mode
+  :defer t
+  :ensure t
+  )
+
+(use-package clang-format
+  :defer t
+  :ensure t
+  :commands (clang-format-buffer clang-format-on-save-mode)
+  :hook (c-mode c++-mode)
+  :config
+  (clang-format-on-save-mode))
+
+;; ---------------------------------------------------------
+;; Themes
+;; ---------------------------------------------------------
 
 ;; Adding nord-theme 
 (add-to-list 'custom-theme-load-path (expand-file-name "~/.config/emacs/themes/nord-theme"))
@@ -34,11 +56,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(magit)))
+ '(package-selected-packages '(clang-format yaml-mode magit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
